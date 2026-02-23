ff4829ee61f3cefc847aff98693ef5fcc795c2b7
Cx <gmer0717@gmail.com>
2026-02-23 23:23:59 +0800

    added a new feature

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const SplashScreen = ({ message }) => (
     position: 'fixed', inset: 0, backgroundColor: '#000',
     display: 'flex', flexDirection: 'column', justifyContent: 'center',
     alignItems: 'center', zIndex: 10000,
-    paddingTop: '15vh'
+    paddingTop: '3vh'
   }}>
     {/* Image zoomed 50% more to 300px */}
     <img src="/mayasplashscreen.jpg" alt="Maya" style={{ width: '53vh', height: 'auto' }} />
