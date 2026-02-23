5666f5a49fad4ed15bb5f291964e75f1b8c75273
Cx <gmer0717@gmail.com>
2026-02-23 23:36:43 +0800

    added a new feature

--- src/App.jsx
+++ src/App.jsx
@@ -439,7 +439,7 @@ const LoginScreen = ({onLogin, fastMode}) => {
         {/* Top Block: Logo, Number, Name, Password Box, Forgot Password */}
         <div style={{
           display:"flex", flexDirection:"column", alignItems:"center", width: "100%", 
-          marginTop: isKeyboardOpen ? "127px" : "24vh",
+          marginTop: isKeyboardOpen ? "127px" : "135px",
           transition: "margin-top 0.3s ease"
         }}>
           
